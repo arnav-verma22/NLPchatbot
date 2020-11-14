027bcade7402d881c61ef633c40f6cc20b062047
arnav-verma22 <arn.verma22@gmail.com>
2020-11-14 15:10:59 +0530

    intents dataset

--- NLPchatbot.py
+++ NLPchatbot.py
@@ -4,6 +4,33 @@ import re
 import nltk
 from nltk.stem.porter import PorterStemmer
 import numpy as np
+import json
+import random
+
+
+with open("intents.json") as file:
+    data=json.load(file)
+
+a = data['intents']
+dict_q = {}
+dict_a = {}
+tags = []
+questions = []
+answers = []
+for intent in a:
+    tags.append(intent['tag'])
+    questions.append(intent['patterns'])
+    answers.append(intent['responses'])
+
+for i in range(6):
+    for j in range(len(questions[i])):
+        dict_q[questions[i][j]] = tags[i]
+
+    for j in range(len(answers[i])):
+        dict_a[answers[i][j]] = tags[i]
+
+index = [k for k in range(len(dict_q))]
+dfq = pd.DataFrame.from_dict(dict_q, orient="index")
 
 df = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
 nltk.download('stopwords')
