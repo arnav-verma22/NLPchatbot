67832749f94d34ac931ddb6b6c7be06c7b801055
arnav-verma22 <arn.verma22@gmail.com>
2020-11-15 20:33:36 +0530

    created dataframe

--- modifiedchatbot.py
+++ modifiedchatbot.py
@@ -11,5 +11,26 @@ import random
 with open("intents.json") as file:
     data=json.load(file)
 
+a = data['intents']
+dict_q = {}
+dict_a = {}
+tags = []
+questions = []
+answers = []
+for intent in a:
+    tags.append(intent['tag'])
+    questions.append(intent['patterns'])
+    answers.append(intent['responses'])
+
+
+for i in range(6):
+    for j in range(len(questions[i])):
+        dict_q[questions[i][j]] = tags[i]
+
+    for j in range(len(answers[i])):
+        dict_a[answers[i][j]] = tags[i]
+
+dfq = pd.DataFrame.from_dict(dict_q, orient="index")
+dfq.reset_index(drop=False, inplace=True)
 
 
